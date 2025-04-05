016d95481b500a84626e88a98ccc9e9fae1967b1
lynnwu830 <83936828+lynnwu830@users.noreply.github.com>
2025-04-06 00:25:44 +0800

    2025

---
+++ taipeicolorchips/dist/script.js
@@ -0,0 +1,58 @@
+$(window).scroll(function(evt){
+  if ($(window).scrollTop()>0)
+    $(".navbar").removeClass("navbar-top");
+  else
+      $(".navbar").addClass("navbar-top");
+});
+
+var s = skrollr.init();
+
+
+// $(window).mousemove(function(evt){
+//    console.log("("+evt.pageX+","+evt.pageY+")")
+//    $(".mousesymbel")
+//      .css("left",evt.pageX+"px")
+//      .css("top",evt.pageY+"px")
+// });
+
+$(".title2, .btn").click(function(evt){
+  console.log(this)
+  var info = $(this).attr("info")
+  $(".shownow").text(info)
+});
+
+$(function(){
+  $(".btn.btn2").click(function(){
+  $(window).scrollTop(800)
+    .animate("transitionDelay","2000")
+           
+  })
+})
+
+
+
+
+
+
+// $(function(){
+//   $(window).scroll(function () {
+//     var scrollVal = $(this).scrollTop()
+//     console.log(scrollVal)
+//     if(scrollVal < 500){
+//       $(".shownow").text("Lynn / Portfolio 2020")
+//     }
+//     else{
+//       $(".shownow").text("Works /")
+//     }
+//   })
+//   });
+
+
+
+
+// $(document).ready(function() {
+//   $(".btn2").click(function() {
+//     $("html, body").animate({
+//       scrollTop: $(".btn2").offset().top }, {duration: 500,easing: "swing"});
+//     return false;
+//   });
